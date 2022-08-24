893ec3858b2232801cfe258c8b8db00be6767617
Tom Rossner <tomrossner2@gmail.com>
2022-08-24 16:21:38 +0300

    Updated

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement("div");
     rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
     rowElement.classList.add("row");
-    guessRow.forEach((guess, guessIndex) => {
+    guessRow.forEach((guessIndex) => {
         const tileElement = document.createElement("div");
         tileElement.setAttribute("id", "guessRow-" + guessRowIndex + "-tile-" + guessIndex);
         tileElement.classList.add("tile");
