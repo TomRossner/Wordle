d06d73b533dced5bd035a48095212894ce69410f
Tom Rossner <tomrossner2@gmail.com>
2022-10-19 11:30:05 +0300

    Cleanup

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import { WORDS } from "./words.js";
-const tilesContainer = document.querySelector(".tiles-container");
 const keyboard = document.querySelectorAll(".kb-button");
 const message = document.querySelector(".message");
 let currentRow = 0;
@@ -18,11 +17,9 @@ const guessRows = [
     ["", "", "", "", ""]
 ];
 const resetButton = document.querySelector(".reset");
-displayMessage("start");
-disableElement(resetButton);
-console.log(correctWord);
 
 guessRows.forEach((guessRow, guessRowIndex) => {
+    const tilesContainer = document.querySelector(".tiles-container");
     const rowElement = document.createElement("div");
     rowElement.setAttribute("id", `guessRow-${guessRowIndex}`);
     rowElement.classList.add("row");
@@ -36,22 +33,9 @@ guessRows.forEach((guessRow, guessRowIndex) => {
 })
 
 
-// Event Listeners
-
-keyboard.forEach((button) => {button.addEventListener("click", checkButton);})
-    
-window.addEventListener("keydown", checkKey);
-
-resetButton.addEventListener("click", () => {
-    const audio = new Audio("./audio/PopSound.mp3");
-    audio.play();
-    reset();
-})
-
-
 // Functions
 
-function checkButton(){ // On-screen keyboard
+const checkButton = () => { // On-screen keyboard
     if(isGameOver === true){return;}
     if(this.innerText.length === 1){
         const audio = new Audio("./audio/PopSound.mp3");
@@ -103,7 +87,7 @@ function checkButton(){ // On-screen keyboard
     }
 }
 
-function checkKey(event){ // Keyboard
+const checkKey = (event) => { // Keyboard
     if(event.key === "Backspace"){
         deleteLetter();
         return;
@@ -157,7 +141,7 @@ function checkKey(event){ // Keyboard
     }
 }
 
-function addLetter(key){
+const addLetter = (key) => {
     if(currentTile < 5 && currentRow < maxGuesses){
         const tile = document.getElementById(`guessRow-${currentRow}-tile-${currentTile}`);
         tile.innerHTML = key;
@@ -172,7 +156,7 @@ function addLetter(key){
     }
 }
 
-function deleteLetter(){
+const deleteLetter = () => {
     if(currentTile > 0){
         currentTile--;
         const tile = document.getElementById(`guessRow-${currentRow}-tile-${currentTile}`);
@@ -183,7 +167,7 @@ function deleteLetter(){
     }
 }
 
-function checkRow(){
+const checkRow = () => {
     let guessedWord = guessRows[currentRow].join("").toLowerCase();
     const rowTiles = document.querySelector(`#guessRow-${currentRow}`).childNodes;
     console.log(guessedWord);
@@ -223,23 +207,26 @@ function checkRow(){
     }else return;
 }
 
-function checkLetters(word){
+const checkLetters = (word) => {
     const rowTiles = document.querySelector(`#guessRow-${currentRow}`).childNodes;
     let delay = 0;
     for(let i = 0; i < word.length ; i++){
         rowTiles[i].style.animation = "letterReveal 1s forwards";
         rowTiles[i].style.animationDelay = `${delay}s`;
         rowTiles[i].style.transitionDelay = `${delay}s`;
-        let guessedLetter = word[i].toLowerCase();
-        let correctLetter = correctWord[i];
         delay += 0.4;
-        setTimeout(() => {
+    }
+    setTimeout(() => {
+        for(let i = 0; i < word.length; i++){
+            let guessedLetter = word[i].toLowerCase();
+            let correctLetter = correctWord[i];
             if(correctWord.includes(guessedLetter) && guessedLetter === correctLetter){
                 setColors("green", rowTiles[i]);
             }
             if(correctWord.includes(guessedLetter) && guessedLetter !== correctLetter){
-                setColors("yellow", rowTiles[i]);
+                setColors("yellow", rowTiles[i])
             }
+        
             if(!correctWord.includes(guessedLetter)){
                 setColors("gray", rowTiles[i]);
             }
@@ -256,8 +243,8 @@ function checkLetters(word){
                     }
                 }
             }, 1500);
-        }, 500);
-    }
+        }
+    }, 500);
     setTimeout(() => {
             if(currentGuess === maxGuesses){
                 setTimeout(() => {
@@ -276,7 +263,7 @@ function checkLetters(word){
         }, 1600);
 }
 
-function setColors(color, tile){
+const setColors = (color, tile) => {
     if(color === "green"){
         tile.classList.add("green");
         
@@ -289,9 +276,8 @@ function setColors(color, tile){
     }
 }
 
-function displayMessage(state){
+const displayMessage = (state) => {
     if(state === "start"){
-        const message = document.querySelector(".message");
         message.textContent = `Start by entering a 5-letter word`;
         message.style.color = "rgb(105, 143, 146)";
         setTimeout(() => {
@@ -301,7 +287,6 @@ function displayMessage(state){
     }
 
     if(state === "game-over"){
-        const message = document.querySelector(".message");
         message.innerHTML = `<p>GAME OVER!<br>The word was ${correctWord.toUpperCase()}</p>`;
         message.style.fontWeight = "bolder";
         message.style.color = "rgb(255, 89, 94)";
@@ -312,7 +297,6 @@ function displayMessage(state){
     }
 
     if(state === "invalid"){
-        const message = document.querySelector(".message");
         message.textContent = `This word doesn't exist`;
         message.style.color = "rgb(255, 89, 94)";
         message.style.animation = `messageDisplay 3s forwards`;
@@ -323,7 +307,6 @@ function displayMessage(state){
     }
 
     if(state === "correct"){
-        const message = document.querySelector(".message");
         message.textContent = `Awesome! You found the word!`;
         message.style.color = "rgb(138, 201, 38)";
         setTimeout(() => {
@@ -333,7 +316,6 @@ function displayMessage(state){
     }
 
     if(state === "correct" && isGameOver === true){
-        const message = document.querySelector(".message");
         message.textContent = `Bravo! You found the word!`;
         message.style.color = "rgb(138, 201, 38)";
         setTimeout(() => {
@@ -343,7 +325,6 @@ function displayMessage(state){
     }
 
     if(state === "short"){
-        const message = document.querySelector(".message");
         message.textContent = `Too short!`;
         message.style.color = "rgb(255, 89, 94)";
         message.style.animation = `messageDisplay 3s forwards`;
@@ -354,7 +335,6 @@ function displayMessage(state){
     }
 
     if(state === "empty"){
-        const message = document.querySelector(".message");
         message.textContent = `You must enter a word!`;
         message.style.color = "rgb(255, 89, 94)";
         message.style.animation = `messageDisplay 3s forwards`;
@@ -365,7 +345,6 @@ function displayMessage(state){
     }
 
     if(state === "already-used"){
-        const message = document.querySelector(".message");
         message.textContent = `You already tried this word!`;
         message.style.color = "rgb(255, 89, 94)";
         message.style.animation = `messageDisplay 3s forwards`;
@@ -376,9 +355,8 @@ function displayMessage(state){
     }
 }
 
-function shakeTiles(){
+const shakeTiles = () => {
     const rowTiles = document.querySelector(`#guessRow-${currentRow}`).childNodes;
-    const tile = document.getElementById(`guessRow-${currentRow}-tile-${currentTile}`);
     for(let i = 0; i < rowTiles.length; i++){
         if(rowTiles[i].classList.contains("filled")){
             rowTiles[i].style.animation = "shakeLetters 0.3s forwards";
@@ -386,7 +364,7 @@ function shakeTiles(){
     }
 }
 
-function reset(){
+const reset = () => {
     currentTile = 0;
     currentRow = 0;
     currentGuess = 0;
@@ -395,7 +373,6 @@ function reset(){
     previousWords = [];
     const filledTiles = document.querySelectorAll(".filled");
     const keyboard = document.querySelectorAll(".kb-button");
-    const message = document.querySelector(".message");
     disableElement(message);
     setTimeout(() => {
         displayMessage("start");
@@ -422,15 +399,33 @@ function reset(){
     resetButton.blur();
 }
 
-function disableElement(element){
+const disableElement = (element) => {
     element.style.pointerEvents = "none";
     element.style.opacity = 0;
     element.style.transition = "opacity 0.2s";
     element.blur();
 }
 
-function enableElement(element){
+const enableElement = (element) => {
     element.style.pointerEvents = "all";
     element.style.opacity = 1;
     element.style.transition = "opacity 0.3s";
 }
+
+
+// Event Listeners
+
+keyboard.forEach((button) => {button.addEventListener("click", checkButton);})
+window.addEventListener("keydown", checkKey);
+resetButton.addEventListener("click", () => {
+    const audio = new Audio("./audio/PopSound.mp3");
+    audio.play();
+    reset();
+})
+
+
+// Init
+
+displayMessage("start");
+disableElement(resetButton);
+console.log(correctWord);
